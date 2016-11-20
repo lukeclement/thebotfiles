13eafb15a6012570abb382de64c4e6c4f41cb516
lukeclement <lclement.57468@farnborough.ac.uk>
2016-11-20 14:48:46 +0000

    x2

--- main.java
+++ main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 import java.text.*;
 import twitter4j.*;
 
@@ -15,6 +16,23 @@ public class main{
 
   public static void main(String[] args) throws TwitterException{
     Twitter twitter=TwitterFactory.getSingleton();
-    
+
+    List<String> people=new ArrayList<>();
+    String fileName="Friends.txt";
+    String lining=null;
+    try{
+      FileReader fileReader = new FileReader(fileName);
+      BufferedReader br=new BufferedReader(fileReader);
+      while((lining=br.readLine())!=null){
+        people.add(lining);
+      }
+      br.close();
+    }catch(Exception ex){}
+
+
+    SimpleDateFormat sdf=new SimpleDateFormat("HH:mm:ss");
+    String startTime=sdf.format(new Date());
+    System.out.println("Initiated at "+startTime);
+
   }
 }
